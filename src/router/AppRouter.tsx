a72c5b1d654abf45a323b4a1ca016f757460e2bc
Axol Dev <axol.dev@gmail.com>
2023-10-03 16:12:53 -0600

    fix routes 3

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
 import { PrivateRouter } from './PrivateRouter'
-import { BrowserRouter as Router } from "react-router-dom";
 
 export const AppRouter = () => {
   return (
